fa8b7f39ce330c6a85e133fd0ad8a42072a78fe4
Sharifa_샤리파 <98262957+Sharifa26@users.noreply.github.com>
2024-12-04 16:08:55 +0000

    voting

--- controller/polls.controller.js
+++ controller/polls.controller.js
@@ -1,6 +1,7 @@
 const Polls = require('../models/polls');
 const InvalidParamException = require('../exceptions/invalid.param.exception');
 const InvalidRequestException = require('../exceptions/invalid-request');
+const { options } = require('../routes/urls');
 
 
 // Create a new poll
@@ -56,4 +57,49 @@ const getAllPolls = (request, response, next) => {
     });
 };
 
-module.exports = { createPoll, getAllPolls };
+// get all polls by user
+const getAllPollsByUser = (request, response, next) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const polls = await Polls.find({ created_by: request.user.userId, is_deleted: false });
+            resolve(
+                response.json({
+                    success: true,
+                    message: 'Polls fetched successfully',
+                    total: polls.length,
+                    polls,
+                })
+            );
+        } catch (error) {
+            next(error);
+        }
+    });
+};
+
+// polls voting
+const pollsVoting = (request, response, next) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const id = request.params.id;
+            const optionIndex = request.body.optionIndex;
+            const polls = await Polls.findById(id);
+            if (!polls) {
+                throw new InvalidParamException('Poll not found');
+            }
+            const voteData = await Polls.updateOne(
+                { _id: id, options: options[optionIndex]  },
+                { options: { votes: votes + 1 } } ,
+            );
+            resolve(
+                response.json({
+                    success: true,
+                    message: 'voted successfully'
+                })
+            );
+        } catch (error) {
+            next(error);
+        }
+    });
+};
+
+module.exports = { createPoll, getAllPolls, getAllPollsByUser, pollsVoting };
--- routes/urls.js
+++ routes/urls.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const { register, login, authenticate, getUser } = require('../controller/users.controller')
-const { createPoll, getAllPolls} = require('../controller/polls.controller');
+const { createPoll, getAllPolls, getAllPollsByUser, pollsVoting} = require('../controller/polls.controller');
 
 router.post('/v2/register', register);
 router.post('/v2/login', login);
 router.get('/v2/user', authenticate, getUser);
 router.post('/v2/polls',authenticate, createPoll);
 router.get('/v2/polls', getAllPolls);
+router.get('/v2/polls/user',authenticate, getAllPollsByUser);
+router.put('/v2/polls/vote/:id', pollsVoting);
 
 module.exports = router
