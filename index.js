981ad94f4701f6165e0802bcc3a4df63dedb04be
Sharifa_샤리파 <98262957+Sharifa26@users.noreply.github.com>
2024-12-09 17:25:36 +0000

    add

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoConnect = require("./db/connections");
 const errorHandlingMiddleware = require("./middleware/errorHandling.middleware");
+const cors = require("cors");
+
 
 
 const app = express();
@@ -15,6 +17,18 @@ app.use("/", require("./routes/urls"));
 
 // Error handling middleware
 app.use(errorHandlingMiddleware);
+
+// Enable CORS for the frontend URL
+const corsOptions ={
+  origin: '*', // Allow the specific frontend URL
+  credentials: true,             // Allow credentials like cookies, authorization headers, etc.
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  optionsSuccessStatus: 204      // Status code for successful OPTIONS requests
+}
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));  // Allow preflight requests for all routes
+
 
 
 // Connect to MongoDB Database
