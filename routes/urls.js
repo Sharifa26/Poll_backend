f3632d5e448971f24d34b4dbb059bdbe6e0bc536
Sharifa26 <ismailsheriff2001@gmail.com>
2024-12-13 22:23:25 +0530

    url

--- routes/urls.js
+++ routes/urls.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { register, login, authenticate, getUser } = require('../controller/users.controller')
+const { register, login, authenticate, getUser, updateUser } = require('../controller/users.controller');
 const { createPoll, getAllPolls, getAllPollsByUser, pollsVoting } = require('../controller/polls.controller');
 
 router.post('/v2/register', register);
 router.post('/v2/login', login);
 router.get('/v2/user', authenticate, getUser);
+router.put('/v2/user/:id', updateUser);
 router.post('/v2/polls', authenticate, createPoll);
 router.get('/v2/polls', getAllPolls);
 router.get('/v2/polls/user', authenticate, getAllPollsByUser);
